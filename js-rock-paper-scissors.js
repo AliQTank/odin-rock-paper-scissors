aa97825cf9211271730d6e57f93d5d3a90a1c48d
alber <aicarq@hotmail.com>
2022-09-22 16:17:23 -0500

    function still to check for input of computer and user, it only checks for coicidence of user choice

--- js-rock-paper-scissors.js
+++ js-rock-paper-scissors.js
@@ -3,13 +3,28 @@
 
 //first step to make function of computerchoice   getComputerChoice
 
-let optionsToChose = ["rock", "paper", "scissors"];
+let optionsToChose = ["Rock", "Paper", "Scissors"];
 
 
 function getComputerChoice(){
     let randomChoiceFormula = Math.floor(Math.random() * optionsToChose.length);
 
-    console.log(optionsToChose[randomChoiceFormula]);
+    return optionsToChose[randomChoiceFormula];
 }
 
 getComputerChoice();
+
+//wirte a function with two parameters, playerselection and computerselection
+
+let playerChoiceIs = "Rock"
+let computerSelected = getComputerChoice();
+
+function singleRound(playerSelection, computerSelection){
+    for (let i = 0; i < optionsToChose.length; i++){
+        if (playerSelection === optionsToChose[i]){
+            console.log("valid answer", `computer chose ${computerSelection}`)
+        } 
+    }
+} 
+
+singleRound(playerChoiceIs, computerSelected);
