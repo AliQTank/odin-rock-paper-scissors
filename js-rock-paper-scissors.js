b881e427d07de01213998bc0eb9dc4c31d2abfac
alber <aicarq@hotmail.com>
2022-09-22 15:27:06 -0500

    getComputerChoice function developed

--- js-rock-paper-scissors.js
+++ js-rock-paper-scissors.js
@@ -1,1 +1,15 @@
 /* lets make this fucking project baby */
+
+
+//first step to make function of computerchoice   getComputerChoice
+
+let optionsToChose = ["rock", "paper", "scissors"];
+
+
+function getComputerChoice(){
+    let randomChoiceFormula = Math.floor(Math.random() * optionsToChose.length);
+
+    console.log(optionsToChose[randomChoiceFormula]);
+}
+
+getComputerChoice();
