d8a9e7496d450de1708215c56cf7fdb10d81e848
alber <aicarq@hotmail.com>
2022-09-23 07:53:37 -0500

    function with both parameters appears to work well

--- js-rock-paper-scissors.js
+++ js-rock-paper-scissors.js
@@ -5,26 +5,56 @@
 
 let optionsToChose = ["Rock", "Paper", "Scissors"];
 
-
 function getComputerChoice(){
     let randomChoiceFormula = Math.floor(Math.random() * optionsToChose.length);
 
     return optionsToChose[randomChoiceFormula];
 }
 
-getComputerChoice();
+//getComputerChoice();
 
 //wirte a function with two parameters, playerselection and computerselection
 
+function singleRound(playerSelection, computerSelection){
+    if (playerSelection === optionsToChose[0] ){
+        switch (computerSelection) {
+            case optionsToChose[1]:
+                console.log("you loose, paper beats rock")
+                break;
+            case optionsToChose[2]:
+                console.log("you win, rock beats scissors")
+                break;
+            default:
+                console.log("Rock solid tie!!");
+        }
+        
+    } else if (playerSelection === optionsToChose[1]){
+        switch (computerSelection) {
+            case optionsToChose[0]:
+                console.log("you win!!, paper beats rock");
+                break;
+            case optionsToChose[2]:
+                console.log("you loose, scissor beats paper");
+                break;
+            default:
+                console.log("it is a tie on paper");            
+        }
+        
+    } else if (playerSelection === optionsToChose[2]){
+        switch (computerSelection) {
+            case optionsToChose[0]:
+                console.log("you loose, rock too solid and destroys scissors");
+                break;
+            case optionsToChose[1]:
+                console.log("you win!!, paper cant take your blade");
+                break;
+            default:
+                console.log("scissor X scissor equals tie");
+        }
+    }
+}
+
+
 let playerChoiceIs = "Rock"
 let computerSelected = getComputerChoice();
-
-function singleRound(playerSelection, computerSelection){
-    for (let i = 0; i < optionsToChose.length; i++){
-        if (playerSelection === optionsToChose[i]){
-            console.log("valid answer", `computer chose ${computerSelection}`)
-        } 
-    }
-} 
-
 singleRound(playerChoiceIs, computerSelected);
