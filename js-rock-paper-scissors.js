22891f7dcbfcc63a211b8ccc6d088505d75b127e
alber <aicarq@hotmail.com>
2022-09-24 14:57:33 -0500

    some html inserted console log changed to return

--- js-rock-paper-scissors.js
+++ js-rock-paper-scissors.js
@@ -1,6 +1,15 @@
 /* lets make this fucking project baby */
 
 
+//SELECTORS SECTION
+const rockChoice = document.querySelector('#ROCK');
+const paperChoice = document.querySelector('#PAPER');
+const scissorsChoice = document.querySelector('#SCISSORS');
+const previaMatch = document.querySelector('.previa-match')
+
+
+
+
 //first step to make function of computerchoice   getComputerChoice
 
 let optionsToChose = ["Rock", "Paper", "Scissors"];
@@ -19,37 +28,37 @@ function singleRound(playerSelection, computerSelection){
     if (playerSelection === optionsToChose[0] ){
         switch (computerSelection) {
             case optionsToChose[1]:
-                console.log("you loose, paper beats rock")
+                return "you loose, paper beats rock";
                 break;
             case optionsToChose[2]:
-                console.log("you win, rock beats scissors")
+                return "you win, rock beats scissors";
                 break;
             default:
-                console.log("Rock solid tie!!");
+                return "Rock solid tie!!";
         }
         
     } else if (playerSelection === optionsToChose[1]){
         switch (computerSelection) {
             case optionsToChose[0]:
-                console.log("you win!!, paper beats rock");
+                return "you win!!, paper beats rock";
                 break;
             case optionsToChose[2]:
-                console.log("you loose, scissor beats paper");
+                return "you loose, scissor beats paper";
                 break;
             default:
-                console.log("it is a tie on paper");            
+                return "it is a tie on paper";            
         }
         
     } else if (playerSelection === optionsToChose[2]){
         switch (computerSelection) {
             case optionsToChose[0]:
-                console.log("you loose, rock too solid and destroys scissors");
+                return "you loose, rock too solid and destroys scissors";
                 break;
             case optionsToChose[1]:
-                console.log("you win!!, paper cant take your blade");
+                return "you win!!, paper cant take your blade";
                 break;
             default:
-                console.log("scissor X scissor equals tie");
+                return "scissor X scissor equals tie";
         }
     }
 }
@@ -62,9 +71,20 @@ choseWisely = choseWisely.charAt(0).toUpperCase() + choseWisely.slice(1);
 let computerSelected = getComputerChoice();
 
 if (choseWisely === optionsToChose[0] || choseWisely === optionsToChose[1] || choseWisely === optionsToChose[2]){
-
-    singleRound(choseWisely, computerSelected);
+    let result = singleRound(choseWisely, computerSelected);
+    console.log(result);
+    previaMatch.textContent = result;
 } else {
     alert("no an option")
 }
+
+
+/* Write a NEW function called game(). 
+Call the playRound function inside of this one to play a 5 round game 
+that keeps score and reports a winner or loser at the end.*/
+
+function game() {
+    
+}
+
 
